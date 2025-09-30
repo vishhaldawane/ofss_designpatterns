dc51242362d2c83ee59bb786b6ac2da5e35dd553
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2025-09-30 15:12:33 +0530

    Memento

---
+++ _25MementoDemo.java
@@ -0,0 +1,83 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class _25MementoDemo {
+	public static void main(String[] args) {
+
+		Originator originator = new Originator();
+		CareTaker careTaker = new CareTaker();
+
+		originator.setState("State #1");
+		originator.setState("State #2");
+		careTaker.add(originator.saveStateToMemento());//new Memento();
+
+		originator.setState("State #3");
+		careTaker.add(originator.saveStateToMemento());//new Memento(state);
+
+		originator.setState("State #4");
+		System.out.println("Current State: " + originator.getState());
+		
+		System.out.println("---------------------");
+
+		originator.getStateFromMemento(careTaker.get(0));
+		System.out.println("First saved State: " + originator.getState());
+		
+		originator.getStateFromMemento(careTaker.get(1));
+		System.out.println("Second saved State: " + originator.getState());
+
+	}
+}
+
+class Memento {
+	private String state;
+
+	public Memento(String state) {
+		this.state = state;
+	}
+
+	public String getState() {
+		return state;
+	}
+}
+
+class Originator {//Notepad
+	private String state;
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	//maintain the snapshot of state in a memento
+	public Memento saveStateToMemento() {
+		return new Memento(state);
+	}
+
+	public void getStateFromMemento(Memento memento) {
+		state = memento.getState();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+class CareTaker {
+	private List<Memento> mementoList = new ArrayList<Memento>();
+
+	public void add(Memento state) {
+		mementoList.add(state);
+	}
+
+	public Memento get(int index) {
+		return mementoList.get(index);
+	}
+}
