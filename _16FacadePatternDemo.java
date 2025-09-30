95d601e6f8902f13366b5e5a20bc8979c53c9dda
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2025-09-30 14:23:10 +0530

    Facade

---
+++ _16FacadePatternDemo.java
@@ -0,0 +1,10 @@
+public class _16FacadePatternDemo {
+   public static void main(String[] args) {
+	   
+      ShapeMaker shapeMaker = new ShapeMaker();
+
+      shapeMaker.drawCircle();
+      shapeMaker.drawRectangle();
+      shapeMaker.drawSquare();		
+   }
+}
